8c40d40118bf1a72d7b533b685cf0003eff1f7c3
Ashutosh Pandey <ashutoshpandeybasti1234@gmail.com>
2026-04-01 19:23:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (58.16%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
+  bool rotatedSort(vector<int>&nums,int target,int si,int ei){
+    if(si>ei) return false;
+        int mid = si +(ei - si)/2;
+                if(nums[mid] == target){ return true ;}
+                if(nums[si] == nums[mid] && nums[mid] == nums[ei]) {
+    return rotatedSort(nums, target, si + 1, ei - 1);
+}
+                if(nums[si]<= nums[mid]){// first line
+                       if(target>=nums[si] && target<nums[mid]){
+                      return rotatedSort(nums, target,si,mid-1);
+                       }
+                       else{
+                        return rotatedSort(nums, target,mid+1,ei);
+                       }
+                }
+                else{ // 2nd line
+                   if(target> nums[mid] && target<=nums[ei]){
+                   return  rotatedSort(nums,target,mid+1,ei);
+                   }
+                   else{
+                   return rotatedSort(nums,target,si,mid-1);
+                   }
+                }
+           }
     bool search(vector<int>& nums, int target) {
-        int count =0;
-        for(int i=0;i<nums.size(); i++){
-            if(nums[i] == target){count ++;}
-        }
-        if(count ==0) return false;
-        else{
-            return true;
-        }
+        // Modified binary search
+         int si =0;
+         int ei = nums.size()-1;
+        return rotatedSort(nums,target,si,ei);
     }
 };
